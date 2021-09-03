4b8119f740cfcf72daef2d295654b20e6a7ffbfa
qiaomingxing <491302297@qq.com>
2021-09-03 16:53:40 +0800

    feat: 优化

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -15,7 +15,7 @@ module.exports = [
   },
   {
     text: "组件",
-    link: "/pages/layout/",
+    link: "/pages/components/layout/",
     items: [
       { text: "Layout - 布局", link: "/pages/components/layout/" },
       { text: "Button - 按钮", link: "/pages/components/button/" },
@@ -31,21 +31,25 @@ module.exports = [
     text: "JS工具类",
     link: "/pages/js/checker/",
     items: [
-      { text: "checker - 表单校验", link: "/pages/js/checker/" },
       {
-        text: '工具类 - Class',
+        text: "checker - 表单校验",
+        items: [{ text: "checker - 表单校验", link: "/pages/js/checker/" }],
+      },
+
+      {
+        text: "工具类 - Class",
         items: [
-          { text: 'Bluetooth - 蓝牙', link: '/pages/class/Bluetooth/' },
-          { text: 'Http - 网络请求', link: '/pages/class/Http/' },
-          { text: 'Mixin - 全局混入', link: '/pages/class/Mixin/' },
-          { text: 'Optimize - 优化类', link: '/pages/class/Optimize/' },
-          { text: 'Pager - 分页', link: '/pages/class/Pager/' },
-          { text: 'Plus - 真机类', link: '/pages/class/Plus/' },
-          { text: 'Store - 状态管理', link: '/pages/class/Store/' },
-          { text: 'StoreModule - 状态管理', link: '/pages/class/StoreModule/' },
-          { text: 'Tips - 提示', link: '/pages/class/Tips/' },
+          { text: "Bluetooth - 蓝牙", link: "/pages/class/Bluetooth/" },
+          { text: "Http - 网络请求", link: "/pages/class/Http/" },
+          { text: "Mixin - 全局混入", link: "/pages/class/Mixin/" },
+          { text: "Optimize - 优化类", link: "/pages/class/Optimize/" },
+          { text: "Pager - 分页", link: "/pages/class/Pager/" },
+          { text: "Plus - 真机类", link: "/pages/class/Plus/" },
+          { text: "Store - 状态管理", link: "/pages/class/Store/" },
+          { text: "StoreModule - 状态管理", link: "/pages/class/StoreModule/" },
+          { text: "Tips - 提示", link: "/pages/class/Tips/" },
         ],
-      }
+      },
     ],
   },
 ];
