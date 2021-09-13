61d6ace47cd9a718b924e7a7b1f5ab4847fe3aa6
qiaomingxing <491302297@qq.com>
2021-09-13 10:54:06 +0800

    feat: public image path

--- assets/js/3.55d90115.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{337:function(t,n,e){},366:function(t,n,e){"use strict";e(337)},372:function(t,n,e){"use strict";e.r(n);var s={},o=(e(366),e(45)),c=Object(o.a)(s,(function(){var t=this.$createElement;return(this._self._c||t)("Content",{staticClass:"demo-model",attrs:{"slot-key":"demo"}})}),[],!1,null,null,null);n.default=c.exports}}]);
